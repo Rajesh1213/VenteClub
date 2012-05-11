112a65f5891d2694709ab09491d8bd404c08062c
Volodymyr Petlovy <vova4kin@list.ru>
2012-05-12 02:53:31 +0300

    top menu initial

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,13 @@ class Event < ActiveRecord::Base
 
   default_scope :order => "end_at ASC"
 
+  scope :past, where("end_at < ?", Date.today)
+
+  #find(:all, :conditions => ["published_at <= ?", Time.now], :include => :comments)
+  #scope :today, find(:all, :conditions => ["start_at <= ? AND end_at > ?", Date.today, Date.today + 2.days])
+  #scope :ending_soon, where("start_at <= ? AND end_at <= ?", [Date.today, Date.today + 2.days])
+  scope :upcoming, where("start_at > ?", Date.today)
+
   validates :top_category_id, :numericality => true
   validates :name, :presence => true, :uniqueness => true
   validates :description, :presence => true
@@ -15,6 +22,15 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :big_image
 
   after_initialize :set_dates
+
+  def self.today
+    find(:all, :conditions => ["start_at <= ? AND end_at > ?", Date.today, Date.today + 2.days])
+  end
+
+  def self.ending_soon
+    find(:all, :conditions => ["end_at >= ? AND end_at <= ?", Date.today, Date.today + 2.days])
+  end
+
 
   private
 
