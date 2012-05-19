74f0a238fbdc6c0a62f96d5fe79dd623fcfb61ad
Volodymyr Petlovy <vova4kin@list.ru>
2012-05-19 13:26:27 +0300

    weird cart issue fix

--- app/assets/javascripts/main/shopping_cart.js
+++ app/assets/javascripts/main/shopping_cart.js
@@ -1,5 +1,50 @@
 $(document).ready(function () {
 
+    var onSelect = false;
+
+    $('.cartTop').live({
+            mouseenter:function () {
+                $('.cartTop').css("background-color", "#C6C7C7");
+                $(".cartData").show();
+            },
+            mouseleave:function () {
+                $('.cartTop').css("background-color", "#ffffff");
+                $(".cartData").hide();
+            }
+        }
+    );
+
+    $('.cartData table').live({
+            mouseenter:function () {
+            },
+            mouseleave:function () {
+                setTimeout(function () {
+                    onSelect = false;
+                }, 0)
+            }
+        }
+    );
+
+    $(".quantitySelect").live("mousedown", function () {
+        onSelect = true;
+    });
+
+    $('.cartData').live({
+            mouseenter:function () {
+                $(".cartTop").css("background-color", "#C6C7C7");
+                $(".cartData").show();
+            },
+            mouseleave:function () {
+                if (onSelect == false) {
+                    $(".cartTop").css("background-color", "#ffffff");
+                    $(".cartData").hide();
+                }
+            }
+        }
+    );
+
+//    cart end
+
     $('.del_product').live("click", function () {
         var id = this.id.split("_")[1];
         $.post("/my/shopping_cart_del/", {authenticity_token:$('meta[name=csrf-token]').attr("content"), id:id},
